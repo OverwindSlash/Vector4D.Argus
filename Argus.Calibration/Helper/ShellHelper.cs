b77037fe62bebfaaddc19ae540dad9ec9ba286de
OverwindSlash <overwind@outlook.com>
2021-05-30 22:04:41 +0800

    Revert "Add shell command stop-able running method"
    This reverts commit 3ab43448e78189c81877f6fb8a8719d4f2129ead.

--- Argus.Calibration/Helper/ShellHelper.cs
+++ Argus.Calibration/Helper/ShellHelper.cs
@@ -86,52 +86,5 @@ namespace Argus.Calibration.Helper
 
             return process.ExitCode;
         }
-
-        public static Process RunKillable(this string cmd, Action action = null)
-        {
-            var source = new TaskCompletionSource<int>();
-            var escapedArgs = cmd.Replace("\"", "\\\"");
-            escapedArgs = escapedArgs.Replace(@"\", @"\\");
-            var process = new Process
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = "bash",
-                    Arguments = $"-c \"{escapedArgs}\"",
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    UseShellExecute = false,
-                    CreateNoWindow = true
-                },
-                EnableRaisingEvents = true
-            };
-
-            process.Exited += (sender, args) =>
-            {
-                if (action != null)
-                {
-                    action();
-                }
-
-                Trace.WriteLine(process.StandardError.ReadToEnd());
-                Trace.WriteLine(process.StandardOutput.ReadToEnd());
-                if (process.ExitCode == 0)
-                {
-                    source.SetResult(0);
-                }
-                else
-                {
-                    source.SetException(new Exception($"Command `{cmd}` failed with exit code `{process.ExitCode}`"));
-                }
-
-                process.Dispose();
-            };
-
-            process.Start();
-
-            process.WaitForExitAsync();
-
-            return process;
-        }
     }
 }
