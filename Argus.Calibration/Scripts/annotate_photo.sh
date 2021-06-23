197bafa7e392dd0a82b42b0cea7ef0b466f04970
Overwind <overwind@outlook.com>
2021-06-23 13:23:42 +0800

    anno photo script bug fix

--- Argus.Calibration/Scripts/annotate_photo.sh
+++ Argus.Calibration/Scripts/annotate_photo.sh
@@ -5,4 +5,4 @@ index=$1
 source /opt/ros/melodic/setup.bash
 echo "corner photo"
 
-gnome-terminal -t "corner photo" -x bash -c "cd ~/RJ1400/lidar/camera_calib && source devel/setup.bash && roslaunch camera_lidar_calibration cornerPhoto.launch image_path:=~/RJ1400/lidar/camera_calib/data/photo/$index.jpg;exec bash"
+gnome-terminal -t "corner photo" -x bash -c "cd ~/RJ1400/lidar/camera_calib && source devel/setup.bash && roslaunch camera_lidar_calibration cornerPhoto.launch image_path:=$HOME/RJ1400/lidar/data/photo/$index.jpg;exec bash"
