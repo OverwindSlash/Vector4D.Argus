cfe6f462eecaaab53dfc6f64c08df06aceffe85f
OverwindSlash <overwind@outlook.com>
2021-06-29 18:13:20 +0800

    Add Lidar user message

--- Argus.Calibration/Views/LidarCalibrationControl.axaml.cs
+++ Argus.Calibration/Views/LidarCalibrationControl.axaml.cs
@@ -22,42 +22,70 @@ namespace Argus.Calibration.Views
 
         private void CheckLidar_OnClick(object? sender, RoutedEventArgs e)
         {
+            var window = (MainWindow)this.Parent.Parent.Parent.Parent;
+            MainWindowViewModel windowDataContext = (MainWindowViewModel)window.DataContext!;
+
+            windowDataContext.AddOperationLog("检查激光雷达，确认后请关闭终端窗口，请勿直接关闭 rviz 窗口");
             "Scripts/check_lidar.sh".Bash();
         }
 
         private void OpenLidarMsg_OnClick(object? sender, RoutedEventArgs e)
         {
+            var window = (MainWindow)this.Parent.Parent.Parent.Parent;
+            MainWindowViewModel windowDataContext = (MainWindowViewModel)window.DataContext!;
+
+            windowDataContext.AddOperationLog("开启激光雷达录制，请勿关闭终端窗口");
+
             "Scripts/open_lidar_msg.sh".Bash();
         }
 
         private void TakePhoto_OnClick(object? sender, RoutedEventArgs e)
         {
+            var window = (MainWindow)this.Parent.Parent.Parent.Parent;
+            MainWindowViewModel windowDataContext = (MainWindowViewModel)window.DataContext!;
+
             var viewModel = (LidarCalibrationControlViewModel)DataContext!;
 
+            windowDataContext.AddOperationLog($"抓拍机载左目图像:{viewModel.CurrentDataCollectIndex}.png ......");            
             $"Scripts/take_photo_for_lidar.sh {viewModel.CurrentDataCollectIndex}".Bash();
+
+            windowDataContext.AddOperationLog("机载左目图像:{viewModel.CurrentDataCollectIndex}.png 抓拍完毕");
         }
 
         private void RecordLidarBag_OnClick(object? sender, RoutedEventArgs e)
         {
+            var window = (MainWindow)this.Parent.Parent.Parent.Parent;
+            MainWindowViewModel windowDataContext = (MainWindowViewModel)window.DataContext!;            
+
             var viewModel = (LidarCalibrationControlViewModel)DataContext!;
 
+            windowDataContext.AddOperationLog("录制激光雷达数据:{viewModel.CurrentDataCollectIndex}.bag ......");
             $"Scripts/record_lidar_bag.sh {viewModel.CurrentDataCollectIndex}".Bash();
-
             viewModel.IncreaceDataCollectIndex();
+            windowDataContext.AddOperationLog("激光雷达数据:{viewModel.CurrentDataCollectIndex}.bag 录制完毕");
         }
 
         private void AnnotatePhoto_OnClick(object? sender, RoutedEventArgs e)
         {
+            var window = (MainWindow)this.Parent.Parent.Parent.Parent;
+            MainWindowViewModel windowDataContext = (MainWindowViewModel)window.DataContext!;
+
             var viewModel = (LidarCalibrationControlViewModel)DataContext!;
 
+            windowDataContext.AddOperationLog("标注机载左目图像:{viewModel.CurrentAnnoPhotoIndex}.png ......");
             $"Scripts/annotate_photo.sh {viewModel.CurrentAnnoPhotoIndex}".Bash();
-
             viewModel.IncreaceAnnoPhotoIndex();
+            windowDataContext.AddOperationLog("机载左目图像:{viewModel.CurrentAnnoPhotoIndex}.png 标注完毕");
         }
 
         private void Convert2Pcd_OnClick(object? sender, RoutedEventArgs e)
         {
+            var window = (MainWindow)this.Parent.Parent.Parent.Parent;
+            MainWindowViewModel windowDataContext = (MainWindowViewModel)window.DataContext!;
+
+            windowDataContext.AddOperationLog("将rosbag转换为pcd点云文件 ......");
             $"Scripts/convert_rosbag_pcd.sh".Bash();
+            windowDataContext.AddOperationLog("pcd点云文件转换完毕");
         }
 
         private void OpenFile_OnClick(object? sender, RoutedEventArgs e)
@@ -67,16 +95,24 @@ namespace Argus.Calibration.Views
 
         private void AnnotateLidar_OnClick(object? sender, RoutedEventArgs e)
         {
+            var window = (MainWindow)this.Parent.Parent.Parent.Parent;
+            MainWindowViewModel windowDataContext = (MainWindowViewModel)window.DataContext!;
+
             var viewModel = (LidarCalibrationControlViewModel)DataContext!;
-
+            windowDataContext.AddOperationLog("标注点云文件:{viewModel.CurrentAnnoLidarIndex}.pcd ......");
             $"Scripts/annotate_lidar.sh {viewModel.CurrentAnnoLidarIndex}".Bash();
-
             viewModel.IncreaceAnnoLidarIndex();
+            windowDataContext.AddOperationLog("点云文件:{viewModel.CurrentAnnoLidarIndex}.pcd标注完毕");
         }
 
         private void GetExt1_OnClick(object? sender, RoutedEventArgs e)
         {
+            var window = (MainWindow)this.Parent.Parent.Parent.Parent;
+            MainWindowViewModel windowDataContext = (MainWindowViewModel)window.DataContext!;
+
+            windowDataContext.AddOperationLog("计算激光雷达与机载相机外参 ......");
             $"Scripts/get_lidar_ext1.sh".Bash();
+            windowDataContext.AddOperationLog("激光雷达与机载相机外参计算完毕");
         }
 
         private void GetExt2_OnClick(object? sender, RoutedEventArgs e)
