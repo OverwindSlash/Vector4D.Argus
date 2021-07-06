4a5e6ab6085ed5e1c8c493b82d1e14506efcaae2
OverwindSlash <overwind@outlook.com>
2021-07-06 13:39:35 +0800

    Add realsense snapshot support

--- Argus.Calibration/Scripts/take_photo_for_lidar.sh
+++ Argus.Calibration/Scripts/take_photo_for_lidar.sh
@@ -5,9 +5,15 @@ index=$1
 source /opt/ros/melodic/setup.bash
 echo "take photo for lidar"
 
-rm -rf ~/RJ1400/lidar/data/photo/left
-rm -rf ~/RJ1400/lidar/data/photo/right
+# for lucid stereo
+#rm -rf ~/RJ1400/lidar/data/photo/left
+#rm -rf ~/RJ1400/lidar/data/photo/right
 
-~/RJ1400/vision/src/Cpp_Save/Cpp_Save ~/RJ1400/lidar/data/photo
+#~/RJ1400/vision/src/Cpp_Save/Cpp_Save ~/RJ1400/lidar/data/photo
 
-cp $(ls ~/RJ1400/lidar/data/photo/left/*.png) ~/RJ1400/lidar/data/photo/$index.png
+#cp $(ls ~/RJ1400/lidar/data/photo/left/*.png) ~/RJ1400/lidar/data/photo/$index.png
+
+# for realsense
+~/RJ1400/vision/src/save-to-disk/rs-save-to-disk ~/RJ1400/lidar/data/photo
+
+cp ~/RJ1400/lidar/data/photo/rs-save-to-disk-output-Color.png ~/RJ1400/lidar/data/photo/$index.png
