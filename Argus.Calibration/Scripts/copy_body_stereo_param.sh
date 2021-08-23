c7c46164ae50fccab21071d637e7593b8b226d85
OverwindSlash <overwind@outlook.com>
2021-08-23 22:23:05 +0800

    Add new body stereo param copy destination

--- Argus.Calibration/Scripts/copy_body_stereo_param.sh
+++ Argus.Calibration/Scripts/copy_body_stereo_param.sh
@@ -6,6 +6,8 @@ sleep 5
 cp -f ~/Vector4D.Argus/Argus.Calibration/bin/Debug/net5.0/CalibrationResults/body_left.yaml ~/.ros/camera_info/body_left.yaml
 cp -f ~/Vector4D.Argus/Argus.Calibration/bin/Debug/net5.0/CalibrationResults/body_right.yaml ~/.ros/camera_info/body_right.yaml
 cp -f ~/Vector4D.Argus/Argus.Calibration/bin/Debug/net5.0/CalibrationResults/body_all.xml ~/.ros/camera_info/body_all.xml
+cp -f ~/Vector4D.Argus/Argus.Calibration/bin/Debug/net5.0/CalibrationResults/body_left.yaml ~/RJ1400/vision/src/arena_camera/camera_info/body_left.yaml
+cp -f ~/Vector4D.Argus/Argus.Calibration/bin/Debug/net5.0/CalibrationResults/body_right.yaml ~/RJ1400/vision/src/arena_camera/camera_info/body_right.yaml
 
 # body stereo param for python
 cp -f ~/Vector4D.Argus/Argus.Calibration/bin/Debug/net5.0/CalibrationResults/body.xml ~/RJ1300_Stereo/Arrester/body.xml
