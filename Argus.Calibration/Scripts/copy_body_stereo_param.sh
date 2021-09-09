dd5aab3890d002f800fe4ff83f4c58b0ec7ab64b
Overwind <overwind@outlook.com>
2021-09-09 09:33:32 +0800

    change stereo param destination name

--- Argus.Calibration/Scripts/copy_body_stereo_param.sh
+++ Argus.Calibration/Scripts/copy_body_stereo_param.sh
@@ -10,5 +10,6 @@ cp -f ~/Vector4D.Argus/Argus.Calibration/bin/Debug/net5.0/CalibrationResults/bod
 cp -f ~/Vector4D.Argus/Argus.Calibration/bin/Debug/net5.0/CalibrationResults/body_right.yaml ~/RJ1400/vision/src/arena_camera/camera_info/body_right.yaml
 
 # body stereo param for python
-cp -f ~/Vector4D.Argus/Argus.Calibration/bin/Debug/net5.0/CalibrationResults/body.xml ~/RJ1300_Stereo/Arrester/body.xml
-cp -f ~/Vector4D.Argus/Argus.Calibration/bin/Debug/net5.0/CalibrationResults/body.xml ~/RJ1300_Stereo/Cable/body.xml
+cp -f ~/Vector4D.Argus/Argus.Calibration/bin/Debug/net5.0/CalibrationResults/body.xml ~/RJ1300_Stereo/Arrester/camera_Link.xml
+cp -f ~/Vector4D.Argus/Argus.Calibration/bin/Debug/net5.0/CalibrationResults/body.xml ~/RJ1300_Stereo/Cable/camera_Link.xml
+cp -f ~/Vector4D.Argus/Argus.Calibration/bin/Debug/net5.0/CalibrationResults/body.xml ~/turntable_install/camera_Link.xml
