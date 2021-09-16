56fe8e23d6550b879d71e0a19fcd3e4d94fed72e
OverwindSlash <overwind@outlook.com>
2021-09-16 19:48:32 +0800

    Bug fix

--- Argus.Calibration/Scripts/update_left_eob_urdf.sh
+++ Argus.Calibration/Scripts/update_left_eob_urdf.sh
@@ -3,11 +3,16 @@
 robot=$1
 preset=$2
 
-if test $[preset] -eq 1
-then
+case $preset in
+0)
+    pos=0
+    ;;
+1)
     pos=a
-else
+    ;;
+2)
     pos=b
-fi
+    ;;
+esac
 
-dotnet ~/RJ1400/calibration/EasyStereoCalibration/UpdateUrdf/bin/Debug/net5.0/UpdateUrdf.dll -u ~/maincontroller/install/share/ur_description/urdf/handeye_calibration_joint.xacro -l ~/.ros/easy_handeye/ur10_leftarm_eye_on_base.yaml -o ~/maincontroller/install/share/ur_description/urdf/handeye_calibration_joint.xacro -b $robot -x ~/jhd/v3/install/share/scene/data/parameters/$robot/turnable/{$pos}_rs_left.txt
+dotnet ~/RJ1400/calibration/EasyStereoCalibration/UpdateUrdf/bin/Debug/net5.0/UpdateUrdf.dll -u ~/maincontroller/install/share/ur_description/urdf/handeye_calibration_joint.xacro -l ~/.ros/easy_handeye/ur10_leftarm_eye_on_base.yaml -o ~/maincontroller/install/share/ur_description/urdf/handeye_calibration_joint.xacro -b $robot -x ~/jhd/v3/install/share/scene/data/parameters/$robot/turnable/${pos}_rs_left.txt
--- Argus.Calibration/Scripts/update_right_eob_urdf.sh
+++ Argus.Calibration/Scripts/update_right_eob_urdf.sh
@@ -3,13 +3,16 @@
 robot=$1
 preset=$2
 
-pos=0
-
-if test $[preset] -eq 1
-then
+case $preset in
+0)
+    pos=0
+    ;;
+1)
     pos=a
-else
+    ;;
+2)
     pos=b
-fi
+    ;;
+esac
 
-dotnet ~/RJ1400/calibration/EasyStereoCalibration/UpdateUrdf/bin/Debug/net5.0/UpdateUrdf.dll -u ~/maincontroller/install/share/ur_description/urdf/handeye_calibration_joint.xacro -r ~/.ros/easy_handeye/ur10_rightarm_eye_on_base.yaml -o ~/maincontroller/install/share/ur_description/urdf/handeye_calibration_joint.xacro -b $robot -x ~/jhd/v3/install/share/scene/data/parameters/$robot/turnable/{$pos}_rs_right.txt
+dotnet ~/RJ1400/calibration/EasyStereoCalibration/UpdateUrdf/bin/Debug/net5.0/UpdateUrdf.dll -u ~/maincontroller/install/share/ur_description/urdf/handeye_calibration_joint.xacro -r ~/.ros/easy_handeye/ur10_rightarm_eye_on_base.yaml -o ~/maincontroller/install/share/ur_description/urdf/handeye_calibration_joint.xacro -b $robot -x ~/jhd/v3/install/share/scene/data/parameters/$robot/turnable/${pos}_rs_right.txt
